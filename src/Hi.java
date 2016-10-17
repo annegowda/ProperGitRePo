ca4ad075a5dec6c4496ac2a3e415b6e6ea46b269
Annegowda <annegowda.cn@storeking.in>
2016-10-17 12:32:37 +0530

    try to switch branch

--- src/Hi.java
+++ src/Hi.java
@@ -4,7 +4,7 @@ public class Hi {
 	public static void main(String[] args) {
 		System.out.println("hi git");
 		System.out.println("hi brandch");
-
+System.out.println("try to switch branch");
 	}
 
 }
